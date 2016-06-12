ccb5ab8abedd28ac61702429b7039480bf58ab40
ferzerkerx <ferzerkerx@gmal.com>
2016-06-12 21:36:17 +0000

    - trying with header forwarded

--- app/service/ReqHeaderService.js
+++ app/service/ReqHeaderService.js
@@ -11,7 +11,7 @@ function ReqHeaderService () {
         if (parenthesisIndex >= 0) {
             userAgent =userAgent.substring(parenthesisIndex + 1, userAgent.indexOf(')'));
         }
-        var whoAmI = {ipaddress: req.ip, language: req.headers['accept-language'].split(",")[0], software: userAgent};
+        var whoAmI = {ipaddress: req.headers['x-forwarded-for'], language: req.headers['accept-language'].split(",")[0], software: userAgent};
 
         res.json(whoAmI);
     };
